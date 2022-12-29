bfdaa9c335f8be28b837666c026959c0bdf2dc33
JavierHornos <hjavih@gmail.com>
2022-12-29 17:29:49 -0300

    probando

--- src/Pages/Categorias.js
+++ src/Pages/Categorias.js
@@ -38,6 +38,7 @@ const Categorias = () => {
                     <td >{cate.nombre}</td>
                     
                     
+                    
                 </tr>
                 
              ))}
