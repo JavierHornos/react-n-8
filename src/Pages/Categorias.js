79d9c8270d6e75c03a62ea6bbf768a64fa2e3d81
FranciscoValenza <franciscovalenza@gmail.com>
2023-01-05 01:39:40 -0300

    cantidad de productos por categoria y armado de dashboard

--- src/Pages/Categorias.js
+++ src/Pages/Categorias.js
@@ -7,10 +7,58 @@ const Categorias = () => {
     
     
     const URL = 'https://digitaldrinks.onrender.com/api/categorias' 
+    const URLCANTIDADES = 'https://digitaldrinks.onrender.com/api/products' 
     const showData = async () => {
         const response = await fetch(URL, {mode:'cors'})
         const data = await response.json()
-        // console.log(data)
+        const responseCantidades = await fetch (URLCANTIDADES, {mode:'cors'} )
+        const dataCantidades =  await responseCantidades.json()
+        const categorias = {
+            1: 'vinos',
+            2: 'espumantes',
+            3: 'whiskies',
+            4: 'gin',
+            5: 'licores',
+            6: 'vodka',
+            7: 'ron',
+            8: 'aperitivos',
+            9: 'cervezas',
+            10: 'accesorios'
+          }
+    
+          const contadorProductosPorCategoria = {
+            vinos: 0,
+            espumantes: 0,
+            whiskies: 0,
+            gin: 0,
+            licores: 0,
+            vodka: 0,
+            ron: 0,
+            aperitivos: 0,
+            cervezas: 0,
+            accesorios: 0
+          }
+    
+         dataCantidades.data.forEach(element => {
+            
+            for (let key in categorias) {
+              if (element.Categoria_FK.toString() === key) {
+                contadorProductosPorCategoria[categorias[key]]++
+              }
+            }
+          });
+  
+        
+        
+        data.data.map(categoria =>{
+            for(let key in contadorProductosPorCategoria){
+                if(categoria.nombre.toLowerCase().includes(key)){
+                    categoria.cant = contadorProductosPorCategoria[key]
+                }
+            }
+            return categoria
+        })
+  
         setCategorias(data.data)
     }
 
@@ -37,7 +85,7 @@ const Categorias = () => {
 
                     <td >{cate.id}</td>
                     <td >{cate.nombre}</td>
-                    <td >{cate.cantidad}</td>
+                    <td >{cate.cant}</td>
                     
                     
                     
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,45 +1,193 @@
-import Carousel from 'react-bootstrap/Carousel';
+//import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import { useEffect, useState } from "react";
+import CantProd from './CantProd'
+import CantUser from './CantUser'
+import CantCategorias from './CantCategorias'
+import Categorias from './Categorias'
+
+
+//function Home() {
+//return (
+/*<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209038/botellas_tgeguh.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Digital Drinks!!!</h3>
+      <p></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209066/segunda_hmyopf.jpg"
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Digital Drinks!!!</h3>
+      <p></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209103/cerveza4_iicbxa.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Digital Drinks!!!</h3>
+      <p></p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+
+ 
+);
+}*/
 
 function Home() {
+
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209038/botellas_tgeguh.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>Digital Drinks!!!</h3>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209066/segunda_hmyopf.jpg"
-          alt="Second slide"
-        />
 
-        <Carousel.Caption>
-          <h3>Digital Drinks!!!</h3>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209103/cerveza4_iicbxa.jpg"
-          alt="Third slide"
-        />
+    <div id="wrapper">
+      {/* Begin Page Content */}
+      <div className="container-fluid">
+        {/* Page Heading */}
+        <div className="d-sm-flex align-items-center justify-content-between mb-4">
+          <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+        </div>
+        {/* Content Row */}
+        <div className="row">
+          {/* Amount of Products in DB */}
+          <div className="col-md-4 mb-4">
 
-        <Carousel.Caption>
-          <h3>Digital Drinks!!!</h3>
-          <p></p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+
+
+
+
+            <div className="h5 mb-0 font-weight-bold text-gray-800">
+              <CantProd />
+            </div>
+
+            <div className="col-auto">
+              <i className="fas fa-clipboard-list fa-2x text-gray-300" />
+            </div>
+
+
+
+          </div>
+          {/* $$$ of all products in DB */}
+          <div className="col-md-4 mb-4">
+
+            <div className="h5 mb-0 font-weight-bold text-gray-800">
+              <CantCategorias />
+            </div>
+          </div>
+          <div className="col-auto">
+            <i className="fas fa-dollar-sign fa-2x text-gray-300" />
+          </div>
+
+          {/* Amount of users in DB */}
+          <div className="col-md-4 mb-4">
+
+            <div className="h5 mb-0 font-weight-bold text-gray-800">
+              <CantUser />
+            </div>
+          </div>
+          <div className="col-auto">
+            <i className="fas fa-user-check fa-2x text-gray-300" />
+          </div>
+
+        </div>
+        {/* Content Row */}
+        <div className="row">
+          {/* Last Product in DB */}
+          <div className="col-lg-6 mb-4">
+            <div className="card shadow mb-4">
+              <div className="card-header py-3">
+                <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
+              </div>
+              <div className="card-body">
+                <div className="text-center">
+                  <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{ width: '25rem' }} src="assets/images/product_dummy.svg" alt="image dummy" />
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
+                <a target="_blank" rel="nofollow" href="/">View product detail</a>
+              </div>
+            </div>
+          </div>
+          {/* Categories in DB */}
+          <div className="col-lg-6 mb-4">
+            <div className="card shadow mb-4">
+              <div className="card-header py-3">
+                <h6 className="m-0 font-weight-bold text-primary">Categories in Data Base</h6>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        <Categorias />
+                      </div>
+
+                    </div>
+                  </div>
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        Category 02
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        Category 03
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        Category 04
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        Category 05
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-info text-white shadow">
+                      <div className="card-body">
+                        Category 06
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* /.container-fluid */}
+    </div>
+
   );
 }
+
+
+
+
+//export default Home;
 
 export default Home;
