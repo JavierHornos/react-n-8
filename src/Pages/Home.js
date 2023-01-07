226321fdb395ec987f9406abcd5e3a71c733ada5
FranciscoValenza <franciscovalenza@gmail.com>
2023-01-07 16:40:16 -0300

    limpiando comentarios

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,50 +7,6 @@ import './home.scss'
 import UltimoProducto from './UltimoProducto'
 import UltimoUsuario from './UltimoUsuario'
 
-//function Home() {
-//return (
-/*<Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209038/botellas_tgeguh.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>Digital Drinks!!!</h3>
-      <p></p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209066/segunda_hmyopf.jpg"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Digital Drinks!!!</h3>
-      <p></p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://res.cloudinary.com/df4wfmkk1/image/upload/v1672209103/cerveza4_iicbxa.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Digital Drinks!!!</h3>
-      <p></p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
- 
-);
-}*/
-
 function Home() {
 
   return (
